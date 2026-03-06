3456469bc6189fa26584271ac50e874f6976535f
themdshakibul <mdshakibul88240@gmail.com>
2026-03-06 20:49:48 +0600

    btn active

--- script/btnactive.js
+++ script/btnactive.js
@@ -1,1 +1,14 @@
-console.log("btn");
+function setActive(clickedBtn) {
+  // all btn
+  const buttons = clickedBtn.parentElement.querySelectorAll("button");
+
+  buttons.forEach((btn) => {
+    // inactive btn
+    btn.classList.remove("btn-primary");
+    btn.classList.add("btn-outline");
+  });
+
+  // Active btn
+  clickedBtn.classList.remove("btn-outline");
+  clickedBtn.classList.add("btn-primary");
+}
